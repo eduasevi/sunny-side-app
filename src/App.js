4cf7924e89870905e3738443a02436d484d09f94
Eduardo Hernandez <eduasevi1990@icloud.com>
2021-10-25 13:52:09 -0700

    specified Icons for weather conditions

--- src/App.js
+++ src/App.js
@@ -5,12 +5,74 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Weather from "./app_component/weather.component";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+// api call api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+const API_key = "7f9265371d936aa19ab675465e4e879e";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celcius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false,
+    };
+    this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
+    };
+  }
+
+  calCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celsius: this.calCelsius(response.main.temp),
+      temp_max: this.calCelsius(response.main.temp_max),
+      temp_min: this.calCelsius(response.main.temp_min),
+      description: response.weather[0].description,
+      icon: this.weatherIcon.Thunderstorm,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+          weatherIcon={this.state.icon}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
