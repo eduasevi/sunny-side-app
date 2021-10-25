8befe4e8a2b23d23fdc1b80cd0e8f8f5f682b6f6
Eduardo Hernandez <eduasevi1990@icloud.com>
2021-10-25 14:20:12 -0700

    started styling form component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "weather-icons/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Weather from "./app_component/weather.component";
+import Form from "./app_component/form.component";
 
 // api call api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
 const API_key = "7f9265371d936aa19ab675465e4e879e";
@@ -40,6 +41,34 @@ class App extends React.Component {
     return cell;
   }
 
+  get_WeatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 531:
+        this.setState({ icon: this.weatherIcon.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: this.weatherIcon.Clouds });
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Clouds });
+    }
+  }
+
   getWeather = async () => {
     const api_call = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
@@ -54,13 +83,14 @@ class App extends React.Component {
       temp_max: this.calCelsius(response.main.temp_max),
       temp_min: this.calCelsius(response.main.temp_min),
       description: response.weather[0].description,
-      icon: this.weatherIcon.Thunderstorm,
     });
+    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
   };
 
   render() {
     return (
       <div className="App">
+        <Form />
         <Weather
           city={this.state.city}
           country={this.state.country}
