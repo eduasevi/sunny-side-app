88675f106077aa5d0251e2efc26f1edbe48b12c9
Eduardo Hernandez <eduasevi1990@icloud.com>
2021-10-25 12:47:17 -0700

    generated app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import "weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Weather from "./app_component/weather.component";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
